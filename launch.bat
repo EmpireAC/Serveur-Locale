16cf1a0408f982624496424d777ca9b6c701a153
Holia <90980259+holia-dev@users.noreply.github.com>
2021-12-27 17:47:30 +0100

    Add files via upload

---
+++ launch.bat
@@ -0,0 +1,25 @@
+:: Empire AC - Serveur-Locale
+:: Par holia-dev
+
+:: 2021 Holia
+:: MIT Licence
+
+@echo off
+title Serveur-Locale - Empire AC
+
+echo [%TIME%] [LOG] Launch Serveur-Locale
+echo [%TIME%] [LOG] Search server.jar
+
+if exist server.jar (
+    goto :ok
+) else (
+    echo [%TIME%] [ERROR] server.jar not found...
+    echo [%TIME%] [ERROR] Serveur-Locale a besoin d'un fichier serveur minecraft en JAR, nommé server.jar
+    echo [%TIME%] [LOG] Exit with code 0.
+    exit
+)
+
+:ok
+echo [%TIME%] [LOG] server.jar has found !
+echo [%TIME%] [LOG] launch server.jar
+java -jar server.jar
